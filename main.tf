d878a70ddf0725424456e5263f6568243e3eb85d
Krishna-2128 <160769906+Krishna-2128@users.noreply.github.com>
2024-11-30 04:01:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,28 +21,3 @@ resource "aws_instance" "app_server" {
     Name = "Terraform_Demo"
   }
 }
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "Terraform_1"
-  acl    = "private"
-}
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-dynamodb1"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-terraform {
-  backend "s3" {
-    bucket         = "Terraform_1"
-    key            = "terraform/statefile.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-dynamodb1"
-  }
-}
